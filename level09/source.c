eb2655d62eed12c1470b6ca9b8b6950ce22f0cf5
Alexandre Hallaine <hallainealexandre@gmail.com>
2024-03-06 19:26:48 +0100

    fix: level09: source.c

--- level09/source.c
+++ level09/source.c
@@ -1,3 +1,4 @@
+// gcc -fno-stack-protector -fpie -pie source.c
 #include <stdio.h>
 #include <string.h>
 
@@ -18,15 +19,34 @@ void secret_backdoor()
 
 void set_msg(t_message *message)
 {
+    char buffer[1024]; // -0x400(%rbp)
+    memset(buffer, 0, 1024);
 
+    puts(">: Msg @Unix-Dude");
+    printf(">>: ");
+
+    fgets(buffer, 1024, stdin);
+
+    strncpy(message->msg, buffer, message->len);
 }
 
 void set_username(t_message *message)
 {
+    char buffer[128]; // -0x80(%rbp)
+    int i; // -0x4(%rbp)
 
+    memset(buffer, 0, 128);
+
+    puts(">: Enter your username");
+    printf(">>: ");
+
+    fgets(buffer, 128, stdin);
+
+    for (i = 0; i <= 40 && buffer[i]; i++)
+        message->user[i] = buffer[i];
+    printf(">: Welcome, %s", message->user);
 }
 
-// slight differences in assembly
 void handle_msg()
 {
     t_message msg; // -0xc0(%rbp)
@@ -36,14 +56,12 @@ void handle_msg()
     set_username(&msg);
     set_msg(&msg);
 
-    // lea instead
     puts(">: Msg sent!");
 }
 
 // slight differences in assembly
 int main()
 {
-    // lea instead
     puts("--------------------------------------------\n|   ~Welcome to l33t-m$n ~    v1337        |\n--------------------------------------------");
     // extra eax to 0
     handle_msg();
