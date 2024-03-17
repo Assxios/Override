6f2e408530a2a0dc0cc8874c9f134af5ada8489c
Alexandre Hallaine <hallainealexandre@gmail.com>
2024-03-17 12:30:04 +0100

    Update: level08 identical source :D

--- level08/source.c
+++ level08/source.c
@@ -1,21 +1,33 @@
+// gcc -z execstack -z now source.c
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
 
-void log_wrapper(FILE *file, const char *str, const char *name)
+void log_wrapper(FILE *file, char *str, char *name)
 {
-	char buffer[264];
+	char buffer[264]; // -0x110(%rbp)
+
 	strcpy(buffer, str);
 	snprintf(&buffer[strlen(buffer)], 254 - strlen(buffer), name);
-
 	buffer[strcspn(buffer, "\n")] = 0;
 	fprintf(file, "LOG: %s\n", buffer);
 }
 
 int main(int argc, char **argv)
 {
+	FILE *log; // -0x88(%rbp)
+	FILE *file; // -0x80(%rbp)
+	int fd; // -0x78(%rbp)
+	char c; // -0x71(%rbp)
+	char dest[104]; // -0x70(%rbp)
+
+	c = -1;
 	if (argc != 2)
 		printf("Usage: %s filename\n", *argv);
 
-	FILE *log = fopen("./backups/.log", "w");
+	log = fopen("./backups/.log", "w");
 	if (!log)
 	{
 		printf("ERROR: Failed to open %s\n", "./backups/.log");
@@ -24,32 +36,25 @@ int main(int argc, char **argv)
 
 	log_wrapper(log, "Starting back up: ", argv[1]);
 
-	FILE *file = fopen(argv[1], "r");
+	file = fopen(argv[1], "r");
 	if (!file)
 	{
 		printf("ERROR: Failed to open %s\n", argv[1]);
 		exit(1);
 	}
-	
-	char dest[104];
+
 	strcpy(dest, "./backups/");
 	strncat(dest, argv[1], 99 - strlen(dest));
-
-	int fd = open(dest, 193, 432);
+	fd = open(dest, 193, 432LL);
 	if (fd < 0)
 	{
 		printf("ERROR: Failed to open %s%s\n", "./backups/", argv[1]);
 		exit(1);
 	}
-
-	while (1)
-	{
-		char buf = fgetc(file);
-		if (buf == -1)
-			break;
-		write(fd, &buf, 1);
-	}
-
+	
+	while ((c = fgetc(file)) != -1)
+		write(fd, &c, 1uLL);
+	
 	log_wrapper(log, "Finished back up ", argv[1]);
 	fclose(file);
 	close(fd);
